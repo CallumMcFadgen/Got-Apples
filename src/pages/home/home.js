adda8244443effe342738149c318edc90e75b6e1
CallumMcFadgen <callum.mcfadgen@gmail.com>
2020-05-17 13:54:50 +1200

    working on UI

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { Row, Col, button } from 'reactstrap';
+import { Row, Col } from 'reactstrap';
 import './../../App.css';
 import './../../pages/home/home.css';
 
@@ -17,21 +17,87 @@ class HomePage extends Component {
 
         {/* NAVIGATION BAR */}
         <div className="nav_style">
-          <Link to="/"><div className="current_nav_button_style">Home</div></Link>
-          <Link to="about"><div className="nav_button_style">About</div></Link>
-          <Link to="growers"><div className="nav_button_style">Growers</div></Link>
-          <Link to="auctions"><div className="nav_button_style">Auctions</div></Link>
-          <Link to="varieties"><div className="nav_button_style">Varieties</div></Link>
-          <Link to="news"><div className="nav_button_style">News</div></Link>
-          <Link to="contact"><div className="nav_button_style">Contact</div></Link>
-          <Link to="login"><div className="nav_button_style">Login</div></Link>
+          <Link to="/"><div className="current_nav_button_style">HOME</div></Link>
+          <Link to="about"><div className="nav_button_style">ABOUT</div></Link>
+          <Link to="growers"><div className="nav_button_style">GROWERS</div></Link>
+          <Link to="auctions"><div className="nav_button_style">AUCTIONS</div></Link>
+          <Link to="varieties"><div className="nav_button_style">VARIETIES</div></Link>
+          <Link to="news"><div className="nav_button_style">NEWS</div></Link>
+          <Link to="contact"><div className="nav_button_style">CONTACT</div></Link>
+          <Link to="login"><div className="nav_button_style">LOGIN</div></Link>
         </div>
 
+        {/* NAVIGATION BREADCRUMBS */}
+        <Row>
+          <Col xs="12" sm="12" md="12" lg={{ size: "10", offset: "1" }} xl={{ size: "10", offset: "1" }}>
+            <div className="bread_crumbs_style">
+              <Link to="/"><span>Home</span></Link>
+            </div>
+          </Col>
+          <span className="crumb_padding"></span>
+        </Row>
+
+        {/* PAGE HEADING */}
+        <Row>
+          <Col xs="12" sm="12" md="12" lg={{ size: "10", offset: "1" }} xl={{ size: "10", offset: "1" }}>
+            <img className="heading_img_style" src="./images/heading_backgrounds/home.png" alt="Green apples" />
+            <h1 className="page_heading_txt">Welcome to Got Apples!</h1>
+          </Col>
+        </Row>
+
+        {/* SPACING */}
+        <Row>
+          <Col xs="12" sm="12" md="12" lg={{ size: "10", offset: "1" }} xl={{ size: "10", offset: "1" }}>
+            &nbsp;
+          </Col>
+        </Row>
 
 
 
 
-        </React.Fragment>
+        <Row>
+          <Col xs="12" sm="12" md="12" lg={{ size: "10", offset: "1" }} xl={{ size: "10", offset: "1" }}>
+            Caresol
+          </Col>
+        </Row>
+
+
+
+
+        {/* SPACING */}
+        <Row>
+          <Col xs="12" sm="12" md="12" lg={{ size: "10", offset: "1" }} xl={{ size: "10", offset: "1" }}>
+            &nbsp;
+          </Col>
+        </Row>
+
+        {/* FOOTER */}
+        <Row className="page_footer_style">
+          <Col className="affiliate_link_style" xs="4" sm="4" md="4" lg="4" xl="4">
+            <div>
+              Links
+            <br />
+            Here
+          </div>
+          </Col>
+          <Col className="footer_logo_style" xs="4" sm="4" md="4" lg="4" xl="4">
+            <div>
+              Logo
+            <br />
+            Here
+          </div>
+          </Col>
+          <Col className="social_media_link_style" xs="4" sm="4" md="4" lg="4" xl="4">
+            <div>
+              Social Media
+          <br />
+          Here
+          </div>
+          </Col>
+        </Row>
+
+        <div className="sub_footer_style"></div>
+      </React.Fragment>
     );
   };
 };
